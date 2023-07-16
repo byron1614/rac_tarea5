e3019819614318477ab8c8584ee869429e9eb5b6
byron <gemechama11203jorge@gmail.com>
2023-07-16 00:33:39 -0600

    se creo el codigo php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta PHP con API Fetch</title>
+</head>
+<body>
+    <h1>Consulta PHP con API Fetch</h1>
+    <form id="formulario">
+        <input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre">
+        <button id="buttonEnviar" >Consultar</button>
+    </form>
+    <p id="resultado"></p>
+
+    <script src="script.js"></script>
+</body>
+</html>

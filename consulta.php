f605c8e9dfb2ba9ea243085e5c21793eb13fa0e2
byron <gemechama11203jorge@gmail.com>
2023-07-16 00:33:04 -0600

    se procedio a realizar el codigo consulta

--- consulta.php
+++ consulta.php
@@ -1,22 +1,10 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Obtener el parámetro 'nombre' enviado por GET
-    $nombre = $_GET['nombre'];
 
-    // Validar que el parámetro 'nombre' no esté vacío
-    if (empty($nombre)) {
-        http_response_code(400); // Solicitud incorrecta
-        echo json_encode("Debe ingresar un nombre");
-        exit;
-    }
-
-    // Realizar alguna acción con el nombre recibido
-    $saludo = "¡Hola, $nombre! Esta es una respuesta desde PHP";
-
-    // Devolver la respuesta en formato JSON
-    echo json_encode($saludo);
+    $saludo = "Este es el método GET. Se ha enviado una solicitud GET al servidor.";
+    echo $saludo;
+    
 } else {
-    http_response_code(405); // Método no permitido
-    echo json_encode("Método no permitido");
+  echo "Este no es el método GET. Se ha enviado una solicitud diferente al servidor.";
 }
 ?>
